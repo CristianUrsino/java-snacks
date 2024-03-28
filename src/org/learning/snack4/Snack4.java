4c920af6d7ee91c1981633f7dbe4bba08227ac20
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-03-28 17:04:09 +0100

    added validation and improved code of Snack4

--- src/org/learning/snack4/Snack4.java
+++ src/org/learning/snack4/Snack4.java
@@ -1,5 +1,6 @@
 package org.learning.snack4;
 //imports
+import java.util.Locale;
 import java.util.Scanner;
 public class Snack4 {
     //Data in input una stringa verificare se è palindroma
@@ -7,8 +8,19 @@ public class Snack4 {
         //dichiaro lo scanner
         Scanner scanner = new Scanner(System.in);
         //dichiaro e inizializzo la stringa in input
-        System.out.print("Please enter the string: ");
-        String inputString = scanner.nextLine();
+        String inputString;
+        do {
+            //ciclo finchè non viene inserita più di una lettera
+            System.out.print("Please enter the string: ");
+            inputString = scanner.nextLine();
+            if(inputString.length() < 2){
+                //se ci sono meno di 2 lettere
+                System.out.println("Errore, the string has less than 2 characters, please try again");
+            }
+        }while(inputString.length() < 2);
+        //formatto la stringa eliminando spazi e maiuscole
+        inputString = inputString.toLowerCase();
+        inputString = inputString.replaceAll(" ", "");
         //dichiaro gli indici e la flag
         int i = 0;
         int j = inputString.length()-1;
@@ -19,10 +31,15 @@ public class Snack4 {
             j--;
             i++;
         }
-        if (isPalindrome) System.out.println("ok");
-        else System.out.println("no");
+        if(isPalindrome){
+            //output finale positivo
+            System.out.println("the string is palindrome");
+        }else {
+            //output finale negativo
+            System.out.println("the string is not palindrome");
+        }
         //chiudo lo scanner
         scanner.close();
     }
-    //ELIMINARE SPAZI NELLA ARRAY IN INPUT
+    //ELIMINARE SPAZI E MAIUSCOLE NELLA ARRAY IN INPUT
 }
