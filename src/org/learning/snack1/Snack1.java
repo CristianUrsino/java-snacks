88bdaed72b43d0e7ff0b51587c9e439fd7d6e9fd
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-03-28 14:22:22 +0100

    complete basic snack1

--- src/org/learning/snack1/Snack1.java
+++ src/org/learning/snack1/Snack1.java
@@ -1,7 +1,27 @@
 package org.learning.snack1;
 
+//imports
+import java.util.Scanner;
+
 public class Snack1 {
     public static void main(String[] args) {
-
+        //definisco scanner
+        Scanner scanner = new Scanner(System.in);
+        //definisco le costanti
+        final int MIN_NUMBER = 10;
+        final int MAX_NUMBER = 20;
+        //dichiaro le variabili
+        int inputNumber;
+        do{
+            //ciclo finchè il numero inserito non è compreso tra MAX e MIN NUMBER
+            System.out.print("Please insert a number between " + MIN_NUMBER + " and " + MAX_NUMBER + ": ");
+            inputNumber = scanner.nextInt();
+            if (inputNumber < MIN_NUMBER || inputNumber > MAX_NUMBER){
+                //se non è compreso stampo output dell'errore
+                System.out.println("number is not between " + MIN_NUMBER + " and " + MAX_NUMBER + ". Please try again ");
+            }
+        }while(inputNumber < MIN_NUMBER || inputNumber > MAX_NUMBER);
+        //output finale
+        System.out.println("thank you");
     }
 }
