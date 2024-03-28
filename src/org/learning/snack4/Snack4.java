c02046ef2d720285cedfd9bfbaf3efd8c3231b3c
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-03-28 15:27:38 +0100

    complete basic snack4

--- src/org/learning/snack4/Snack4.java
+++ src/org/learning/snack4/Snack4.java
@@ -1,7 +1,28 @@
 package org.learning.snack4;
-
+//imports
+import java.util.Scanner;
 public class Snack4 {
+    //Data in input una stringa verificare se è palindroma
     public static void main(String[] args) {
-
+        //dichiaro lo scanner
+        Scanner scanner = new Scanner(System.in);
+        //dichiaro e inizializzo la stringa in input
+        System.out.print("Please enter the string: ");
+        String inputString = scanner.nextLine();
+        //dichiaro gli indici e la flag
+        int i = 0;
+        int j = inputString.length()-1;
+        boolean isPalindrome = true;
+        //ciclo l'array e verifico che ogni lettera sia uguale se letta al contrario
+        while(i<inputString.length() && isPalindrome){
+            if(inputString.charAt(i) != inputString.charAt(j)) isPalindrome = false;
+            j--;
+            i++;
+        }
+        if (isPalindrome) System.out.println("ok");
+        else System.out.println("no");
+        //chiudo lo scanner
+        scanner.close();
     }
+    //ELIMINARE SPAZI NELLA ARRAY IN INPUT
 }
