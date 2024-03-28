80466bc3c9e1396572d503e8085dc6791681f086
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-03-28 15:47:46 +0100

    complete basic snack5

--- src/org/learning/snack5/Snack5.java
+++ src/org/learning/snack5/Snack5.java
@@ -1,7 +1,36 @@
 package org.learning.snack5;
-
+//imports
+import java.util.Scanner;
 public class Snack5 {
+    //Data una stringa in input mostrare a video quanti caratteri alfabetici contiene,
+    // quanti numeri e quanti simboli non alfanumerici.
     public static void main(String[] args) {
-
+        //definisco lo scanner
+        Scanner scanner = new Scanner(System.in);
+        //dichiaro e inizializzo le variabili
+        int characters = 0, numbers = 0, symbols = 0;
+        //dichiaro e inizializzo la stringa
+        System.out.print("please enter the string: ");
+        String inputString = scanner.nextLine();
+        //ciclo i caratteri della stringa e incremento le variabili
+        for (int i = 0; i < inputString.length(); i++) {
+            if(Character.isAlphabetic(inputString.charAt(i))){
+                //se il seguente carattere è un carattere alfabetico
+                characters++;
+            }else if(Character.isDigit(inputString.charAt(i))){
+                //se il seguente carattere è un numero
+                numbers++;
+            }else{
+                //se non è un numero o un carattere alfanumerico è un simbolo speciale
+                symbols++;
+            }
+        }
+        //input finale
+        System.out.println(
+                "\n the string contains " + characters + " alphabetical characters" +
+                "\n the string contains " +  + numbers + " numbers" +
+                        "\n the string contains " +  + symbols + " special characters");
+        //chiudo lo scanner
+        scanner.close();
     }
 }
